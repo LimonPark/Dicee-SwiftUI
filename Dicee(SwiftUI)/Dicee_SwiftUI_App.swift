b085b945f1cc22b145e725ba7b9e6b347a25a3cc
nikita <kafes-nik@mail.ru>
2023-09-13 16:10:19 +0300

    Initial Commit

---
+++ Dicee(SwiftUI)/Dicee_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_App.swift
+//  Dicee(SwiftUI)
+//
+//  Created by Nikita Hasan Kafes on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
